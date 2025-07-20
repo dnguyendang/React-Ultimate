22799cdf7fd00849535d38e5a3b514fae6944020
Dangg Dungg <dungduongthanhson@gmail.com>
2025-07-20 22:34:04 +0700

    Update: UserPage; useState, useEffect

---
+++ 01-react-vite-hoidanit-starter-master/src/components/user/user.form.jsx
@@ -0,0 +1,70 @@
+import { Button, Descriptions, Input, notification } from "antd";
+import { useState } from "react";
+import axios from "axios"
+import { createUserAPI } from "../../services/api.service";
+
+
+const UserForm = () => {
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phone, setPhone] = useState("")
+
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone)
+        if (res?.data) {
+            notification.success({
+                message: "create user",
+                description: "Tạo user thành công"
+            })
+        } else {
+            notification.error({
+                message: "Error create user",
+                description: JSON.stringify(res.message)
+            })
+        }
+    }
+
+    return (
+        <div className="user-form" style={{ margin: "20px 0" }}>
+            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                <div>
+                    <span>Full Name</span>
+                    <Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Phone Number</span>
+                    <Input
+                        value={phone}
+                        onChange={(event) => { setPhone(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <Button
+                        onClick={handleClickBtn}
+                        type="primary"
+                    >Create User</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserForm;
---
+++ 01-react-vite-hoidanit-starter-master/src/components/user/user.table.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Space, Table, Tag } from 'antd';
+import { fetchAllUserAPI } from '../../services/api.service';
+
+
+const UserTable = () => {
+
+    const [dataUsers, setDataUsers] = useState([])
+    useEffect(() => {
+        loadUser();
+    }, []);
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: '_id',
+        },
+        {
+            title: 'Full Name',
+            dataIndex: 'fullName',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+        },
+        {
+            title: 'Phone Number',
+            dataIndex: 'phone',
+        },
+    ];
+
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI()
+        setDataUsers(res.data)
+    }
+
+    return (
+        <Table
+            columns={columns}
+            dataSource={dataUsers}
+            rowKey={"_id"}
+        />
+    )
+}
+
+export default UserTable;
+
+
