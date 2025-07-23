f456a14bc2781185bc490f2bd3ff006cb8d565d6
dungduongthanhson <dungduongthanhson@gmail.com>
2025-07-23 17:17:40 +0700

    Update: Form register

--- 01-react-vite-hoidanit-starter-master/src/pages/register.jsx
+++ 01-react-vite-hoidanit-starter-master/src/pages/register.jsx
@@ -1,7 +1,74 @@
 
+import { Button, Form, Input } from 'antd'
+
 const RegisterPage = () => {
+    const [form] = Form.useForm()
+    const onFinish = (values) => {
+        console.log(">>> check : ", values)
+    }
     return (
-        <div>register page</div>
+        <Form
+            form={form}
+            layout="vertical"
+            onFinish={onFinish}
+        >
+            <div style={{ margin: "50px" }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your full name!'
+                        }
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your email!'
+                        }
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!'
+                        }
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+                <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                    rules={[
+                        {
+                            pattern: new RegExp(/\d+/g),
+                            message: 'Wrong format!'
+                        }
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button
+                        type='primary'
+                        onClick={() => form.submit()}
+                    >Register</Button>
+                </div>
+            </div>
+        </Form>
     )
 }
 
